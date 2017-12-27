71aa6b64bc61c6e92463411b47a30b917b8a540e
dvtung1 <d.v.tungpro@gmail.com>
2017-12-27 23:02:27 +0700

    add comparing badwords in lowercase in text file

--- ChatFilter.java
+++ ChatFilter.java
@@ -11,11 +11,14 @@ public class ChatFilter {
             FileReader fr = new FileReader(file);
             BufferedReader bfr = new BufferedReader(fr);
             int lineCount =0;
+
+            //Count how many lines exist in the text file
             while(bfr.readLine() != null){
                 lineCount++;
             }
             bfr.close();
 
+            //copy badwords in each lines in text file to string array
             String[] array = new String[lineCount];
             FileReader fr2 = new FileReader(file);
             BufferedReader bfr2 = new BufferedReader(fr2);
@@ -23,11 +26,17 @@ public class ChatFilter {
                 array[i] = bfr2.readLine();
             }
             bfr2.close();
+
+            /*
+            *Compare client's message to those badwords in the array
+            *If it is match, the badwords inside the message will appear as "*". 
+            *The amount of "*" will be corresponding to number of letters in the badword
+            */
             for(int k =0; k < array.length; k++){
                 int j =0;
                 for(int i = array[k].length(); i <= msg.length(); i++) {
                     String badWord = msg.substring(j, i).toLowerCase();
-                    if(badWord.equals(array[k])){
+                    if(badWord.equals(array[k].toLowerCase())){ 
                         String wordBefore = msg.substring(0, j);
                         String wordAfter = msg.substring(i, msg.length());
                         String newWord = "*";
