aaf22faca6c2444479a8c8fb65e6f9d07ddb4601
dvtung1 <d.v.tungpro@gmail.com>
2017-12-27 22:58:07 +0700

    remove randomUsername, send client a message when directmessage to themselves, add more comments

--- ChatServer.java
+++ ChatServer.java
@@ -9,19 +9,17 @@ import java.util.Date;
 import java.util.List;
 
 final class ChatServer {
-    private static String fileAddress;
     private static int uniqueId = 0;
     private final List<ClientThread> clients = new ArrayList<>();
     private final int port;
     private static String fileAddress;
-    private static int randomNumber =0;
     private ChatServer(int port) {
         this.port = port;
     }
 
     /*
      * This is what starts the ChatServer.
-     * Right now it just creates the socketServer and adds a new ClientThread to a list to be handled
+     * Check if the username has been used. If it is, send a message to the client and do not add him/her to the ClientThread  
      */
     private void start() {
         try {
@@ -30,21 +28,35 @@ final class ChatServer {
                 Socket socket = serverSocket.accept();
                 Runnable r = new ClientThread(socket, uniqueId++);
                 Thread t = new Thread(r);
-                clients.add((ClientThread) r);
-                t.start();
+                boolean check = false;
+                for(int i=0; i < clients.size(); i++){
+                    if(((ClientThread) r).username.equals(clients.get(i).username)){
+                        check = true;
+                    }
+                }
+                if(check == false) {
+                    clients.add((ClientThread) r);
+
+                    t.start();
+                }else{
+                    uniqueId--;
+                }
+
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    //function to broadcast to all clients, including server's window
     private synchronized void broadcast(String message){
-              message = getDate()+" "+message;
+        message = getDate()+" "+message;
         for(int i = 0; i<clients.size(); i++){
             ClientThread cli = clients.get(i);
             cli.writeMessage(message);
         }
         System.out.println(message);
     }
+    //function to send private message to a client and also print it to server's window
     private synchronized void directMessage(String message, String username){
         message = getDate()+" "+message;
         boolean found = false;
@@ -57,15 +69,15 @@ final class ChatServer {
         }
         if(found)
             System.out.println(message);
-        else
-            System.out.println("Person not found.");
+       
     }
+    //formatting for the message
     private String getDate(){
         Date date = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
         return formatter.format(date);
     }
-
+    //remove client from the ClientThread
     private synchronized void remove(int id){
         clients.remove(id);
     }
@@ -104,11 +116,13 @@ final class ChatServer {
             try {
                 sOutput = new ObjectOutputStream(socket.getOutputStream());
                 sInput = new ObjectInputStream(socket.getInputStream());
+                //read the username sent by client
                 username = (String) sInput.readObject();
                 for(int i =0; i < clients.size(); i++) {
                     if (username.equals(clients.get(i).username)){
-                        username = "randomUsername" + randomNumber;
-                        randomNumber++;
+
+                        writeMessage("The username has been used! Please sign up with another username");
+
                     }
                 }
             } catch (IOException | ClassNotFoundException e) {
@@ -125,13 +139,16 @@ final class ChatServer {
          */
         @Override
         public void run() {
-            // Read the username sent to you by client
            broadcast(username+" just connected.");
             boolean x = true;
             while(x) {
                 try {
                     ChatFilter chatFilter = new ChatFilter(fileAddress);
+                    
+                    //read all ChatMessage objects sent from Client class
                     cm = (ChatMessage) sInput.readObject();
+
+                    //remove client from the ClientThread when client type "/logout" => see more in ChatClient file 
                     if (cm.getType() == 1) {
                         x = false;
                         if(clients.size()>=id+1) {
@@ -139,6 +156,7 @@ final class ChatServer {
                                     "message.");
                             remove(id);
                         }
+                    //show all other online users when client type "/list"
                     }else if (cm.getMessage().toLowerCase().equals("/list")){
                         writeMessage("Other online clients: ");
                         for(int i =0; i < clients.size(); i++) {
@@ -147,23 +165,39 @@ final class ChatServer {
                             }
                         }
                     }
+                    //broad cast regular message
                     else if(cm.getType() == 0){
                         if(fileAddress!=null)
                             broadcast(username+": "+chatFilter.filter(cm.getMessage()));
                         else
                             broadcast(username+": "+cm.getMessage());
                     }
+                    //send private (direct) message
                     else if(cm.getType() == 2){
-                        if(fileAddress!=null) {
-                            directMessage(username + "->" + cm.getRecipient() + ": " + chatFilter.filter(cm.getMessage()), cm.getRecipient());
-                            writeMessage(username + "->" + cm.getRecipient() + ": " + chatFilter.filter(cm.getMessage()));
-                        }
-                        else{
-                            directMessage(username + "->" + cm.getRecipient() + ": " + cm.getMessage(), cm
-                                    .getRecipient());
-                            writeMessage(username + "->" + cm.getRecipient() + ": " + cm.getMessage());
+                        boolean check = false;
+			                for(int i =0; i < clients.size(); i++){
+			                    if(cm.getRecipient().equals(clients.get(i).username)){
+			                        check = true;
+			                        if(fileAddress!=null) {
+                                        directMessage(username + "->" + cm.getRecipient() + ": " + chatFilter.filter(cm.getMessage()), cm.getRecipient());
+                                        writeMessage(username + "->" + cm.getRecipient() + ": " + chatFilter.filter(cm.getMessage()));
+                                    }else{
+                                        directMessage(username + "->" + cm.getRecipient() + ": " + cm.getMessage(), cm
+                                                .getRecipient());
+                                        writeMessage(username + "->" + cm.getRecipient() + ": " + cm.getMessage());
+                                    }
+                                }
+                            }
+                        //notice client when sending a direct message to an unknown client
+                        if (check == false){
+                            writeMessage("Person is not found.");
                         }
                     }
+                    //notice client when sending a direct message to him/herself
+                    else if(cm.getType() == 3){
+                        writeMessage("You cannot send private message to yourself!");
+                    }
+                    //remove client from ClientThread when the connection between server and client is interrupted
                 } catch (IOException | ClassNotFoundException e) {
                     if(clients.size()>=id+1) {
                         broadcast(username+" has disconnected.");
@@ -173,6 +207,7 @@ final class ChatServer {
                 }
             }
         }
+        //send string message as Object to Client class
         private boolean writeMessage(String msg){
             try {
                 sOutput.writeObject(msg);
